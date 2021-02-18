7a824a9d68188b652577717ff84da9aa8daec1c8
Stack-Pointer-485 <63495121+Stack-Pointer-485@users.noreply.github.com>
2021-02-18 22:49:02 +0530

    Update index.js
    Added function to collect data from p tag

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+document.getElementById("click");
+
+function click(){
+   document.getElementById("para").innerHtml;
+}
